8998a9f473ecda9d99d17e3fc384b286b5e997c9
ChampMo <97272854+ChampMo@users.noreply.github.com>
2023-11-20 21:57:49 +0700

    .

--- public/js/profile_script.js
+++ public/js/profile_script.js
@@ -230,3 +230,31 @@ document.querySelector('.c_button').addEventListener('click', function() {
     edit_address_button.style.display = 'flex';
     xc_button.style.display = 'none';
 });
+
+
+//---------------------------------------
+function handleFileUpload() {
+    // Trigger click on hidden file input
+    document.getElementById('fileInput').click();
+
+    // Listen for file input change
+    document.getElementById('fileInput').addEventListener('change', function() {
+        // Get the selected file
+        const file = this.files[0];
+
+        // Do something with the file (e.g., upload it)
+        if (file) {
+            uploadFile(file);
+        }
+    });
+}
+
+function uploadFile(file) {
+    // You can implement your file upload logic here
+    console.log('Uploading file:', file.name);
+    // Add your code to handle the file upload, e.g., using AJAX or FormData
+    // Example:
+    // const formData = new FormData();
+    // formData.append('file', file);
+    // Use AJAX or another method to send the formData to the server
+}
