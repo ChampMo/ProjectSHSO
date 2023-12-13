61e23b53dfb8e49d217569abb7853a622ad0cc4e
ChampMo <97272854+ChampMo@users.noreply.github.com>
2023-12-14 00:15:16 +0700

    .

--- public/js/main_script.js
+++ public/js/main_script.js
@@ -69,6 +69,81 @@ function createProductBox(productId) {
 
 
 
+function search_product(){
+    const input1 = document.querySelector('.input1').value;
+    if (input1 !== ''){
+        window.location.href = '/main'
+            productContainer.innerHTML = '';
+            fetch('/search-product/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ input1 }),
+            })
+            .then(response => response.json())
+            
+            .then(totalProducts => {
+                // Create product boxes based on the total count
+                
+                totalProducts.forEach(element => {
+                    createProductBoxsearch(element.product_id);
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching product data:', error);
+            });
+        }
+}
+
+    
+    
+
+
+
+function createProductBoxsearch(productId) {
+    // สร้าง .shop_pro
+    const product_box = document.createElement('a');
+    product_box.className = 'inproduct';
+    product_box.id = `shop${productId}`;
+
+    // ส่งคำขอ GET ไปยัง server เพื่อดึงข้อมูล
+    fetch('/api/products-search/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ productId }),
+    })
+        .then(response => response.json())
+        .then(product => {
+            const name_product = product[0].name
+            const price_product = product[0].price
+            const id_product = product[0].product_id
+            const picture1 = product[0].picture1
+            product_box.href = `/product/${id_product}`;
+            console.log(name_product)
+            product_box.innerHTML = `
+                    <div class="imgproduct_box"><img src="${picture1}"></div>
+                    <div class="detail_product_box">
+                        ${name_product}
+                    </div> 
+                    <div class="cost_product">
+                        ${formatNumber(price_product)}
+                    </div>`
+                    ;
+
+        })
+    
+        .catch(error => {
+            console.error('Error fetching product data:', error);
+        });
+
+    const productContainer = document.getElementById('productContainer');
+    productContainer.appendChild(product_box);
+}
+
+
 
 
 
